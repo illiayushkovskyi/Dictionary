1c8ad752e6c7772ed93e69f62305ddc6d66fbea1
Illia Yushkovskyi <haishtipil@gmail.com>
2022-09-24 15:35:32 +0200

    Добавьте файлы проекта.

---
+++ Cars1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars1
+{
+    class CarColection<year, model>
+    {
+        KeyValuePair<year, model>[] car = new KeyValuePair<year, model>[0];
+
+        public void Add(year year, model value)
+        {
+            KeyValuePair<year, model>[] newcar = new KeyValuePair<year, model>[car.Length + 1];
+
+            for (int i = 0; i < car.Length; i++)
+            {
+                newcar[i] = car[i];
+            }
+            newcar[newcar.Length - 1] = new KeyValuePair<year, model>(year, value);
+            car = newcar;
+        }
+        public int Count
+        {
+            get { return car.Length; }
+        }
+        public KeyValuePair<year, model> this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < car.Length)
+                    return car[index];
+                return default;
+            }
+        }
+        public void Clear()
+        {
+            if (car.Length > 0)
+            {
+                car = null;
+            }
+        }
+        public KeyValuePair<year, model> this[model text]
+        {
+            get
+            {
+                for (int i = 0; i < car.Length; i++)
+                {
+                    if (car[i].Value.Equals(text))
+                    {
+                        return car[i];
+                    }
+                }
+                return default(KeyValuePair<year, model>);
+            }
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarColection<int, string> myCars = new CarColection<int, string>();
+            myCars.Add(2004, "Mazda3");
+            myCars.Add(1999, "BMW 320");
+            myCars.Add(2008, "Peugeot 307cc");
+
+            Console.WriteLine(myCars[0]);
+
+            for (int i = 0; i < myCars.Count; i++)
+            {
+                Console.WriteLine(myCars[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(48, "my rabbit");
+            dic.Add(20, "my pig");
+            dic.Add(18, "my pony");
+
+            Console.WriteLine(dic[48]);
+
+            for (int i = 0; i < dic.Count; i++)
+            {
+                Console.WriteLine(dic[i]);
+            }
+
+            Console.ReadKey();
+        }
+        class Dictionary<index,content>
+        {
+            KeyValuePair<index, content>[] kvp = new KeyValuePair<index, content>[0];
+           
+            public void Add(index index, content value)
+            {
+                KeyValuePair<index, content>[] newkvp = new KeyValuePair<index, content>[kvp.Length + 1];
+
+                for (int i = 0; i < kvp.Length; i++)
+                {
+                    newkvp[i] = kvp[i];
+                }
+                newkvp[newkvp.Length - 1] = new KeyValuePair<index, content>( index, value );
+                kvp = newkvp;
+            }
+            public int Count
+            {
+                get { return kvp.Length; }
+            }
+            public KeyValuePair<index, content> this[int index1]
+            {
+                get
+                {
+                    if(index1>=0&& index1 < kvp.Length)
+                        return kvp[index1];
+                    return default;
+                }
+            }
+            public KeyValuePair<index, content> this[index number]
+            {
+                get
+                {
+                    for (int i = 0; i < kvp.Length; i++)
+                    {
+                        if (kvp[i].Key.Equals(number))
+                        {
+                            return kvp[i];
+                        }
+                    }
+                    return default(KeyValuePair<index, content>);
+                }
+            }
+            public KeyValuePair<index, content> this[content text]
+            {
+                get
+                {
+                    for (int i = 0; i < kvp.Length; i++)
+                    {
+                        if (kvp[i].Value.Equals(text))
+                        {
+                            return kvp[i];
+                        }
+                    }
+                    return default(KeyValuePair<index, content>);
+                }
+            }
+        }
+    }
+}
